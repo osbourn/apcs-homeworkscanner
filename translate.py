15d41abfa387a41d044d7fc8dc288d4c2e800145
Niels Voss <code@nielsvoss.com>
2023-08-26 15:20:32 -0700

    Format translate.py

--- translate.py
+++ translate.py
@@ -1,58 +1,56 @@
-def Translate(start) : 
-      end = "";
-      arr = start.split(" ") 
-      problem = ""
-      for x in arr:
-      	y = x.lower()
-      	num = 0
-      	#.len()
-      	#check for problem numbers
-      	if problem != "":
-      		if "," in y: 
-      			y = x[0:-1]
+def Translate(start):
+    end = ""
+    arr = start.split(" ")
+    problem = ""
+    for x in arr:
+        y = x.lower()
+        num = 0
+        # .len()
+        # check for problem numbers
+        if problem != "":
+            if "," in y:
+                y = x[0:-1]
 
-      		#print(y)
-      		try :
-      			#print("no")
-      			num = float(y) 
-      			#print(num)
-      		except : 
-      			#print("whoops")
-      			pass
+            # print(y)
+            try:
+                # print("no")
+                num = float(y)
+            # print(num)
+            except:
+                # print("whoops")
+                pass
 
-      	#reformat numbers
-      		if num != 0 and len(y) < 4: 
-      			#print("hi")
-      			i = y.index(".")+1
-      			z = (y[0:i] + "0" + y[i])
-      			num = float(z) 
-      			#print(num)
+            # reformat numbers
+            if num != 0 and len(y) < 4:
+                # print("hi")
+                i = y.index(".") + 1
+                z = (y[0:i] + "0" + y[i])
+                num = float(z)
+            # print(num)
 
+        # add translated problem to string
+        if problem == "sc" and y != "ex":
+            # print("Hi")
+            end += ("Self-Check " + str(num) + " \n")
+        if problem == "ex" and y != "sc":
+            # print("Hi")
+            end += ("Exercise " + str(num) + " \n")
 
-      	#add translated problem to string
-      	if problem == "sc" and y != "ex":
-      		#print("Hi")
-      		end += ("Self-Check " + str(num) +" \n")
-      	if problem == "ex" and y != "sc":
-      		#print("Hi")
-      		end += ("Exercise " + str(num) +" \n")
+        # determine problem type
+        if y == "sc":
+            problem = "sc"
+        if y == "ex":
+            problem = "ex"
 
-      	# determine problem type
-      	if y == "sc" :
-      		problem = "sc"
-      	if y == "ex" :
-      		problem = "ex"
-
-      return end[-1]
-
+    return end[-1]
 
 
- #write translated text to questions?
+# write translated text to questions?
 end = ""
 f = open("raw.txt", "r")
 start = f.read()
 
-#print(start)
+# print(start)
 end = Translate(start)
 print(end)
 
